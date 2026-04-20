d43314efe2d0a505ead967f21459f8fcb57216a0
Mike Makamo <thandomakamo98@gmail.com>
2026-04-20 03:17:29 +0200

    css update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"BPPwXJlEPBHuoqj59gKOkC3UA/4A6eh6xHPLTmi36m4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
